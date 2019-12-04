208e1f35c8c234bd1ff32e99cea73312521563e0
Gert Jan Bossink <gertjb73@hotmail.com>
2019-12-04 01:18:56 +0100

    Day 3 scripts added

--- 2019/Day3_Part2.py
+++ 2019/Day3_Part2.py
@@ -1,32 +1,14 @@
 # --- Day 3: Crossed Wires ---
-# --- Part One ---
+# --- Part Two ---
 #
-# The gravity assist was successful, and you're well on your way to the Venus refuelling station. During the rush
-# back on Earth, the fuel management system wasn't completely installed, so that's next on the priority list.
+# It turns out that this circuit is very timing-sensitive; you actually need to minimize the signal delay.
 #
-# Opening the front panel reveals a jumble of wires. Specifically, two wires are connected to a central port and
-# extend outward on a grid. You trace the path each wire takes as it leaves the central port, one wire per line of
-# text (your puzzle input).
+# To do this, calculate the number of steps each wire takes to reach each intersection; choose the intersection where
+# the sum of both wires' steps is lowest. If a wire visits a position on the grid multiple times, use the steps value
+# from the first time it visits that position when calculating the total value of a specific intersection.
 #
-# The wires twist and turn, but the two wires occasionally cross paths. To fix the circuit, you need to find the
-# intersection point closest to the central port. Because the wires are on a grid, use the Manhattan distance for
-# this measurement. While the wires do technically cross right at the central port where they both start, this point
-# does not count, nor does a wire count as crossing with itself.
-#
-# For example, if the first wire's path is R8,U5,L5,D3, then starting from the central port (o), it goes right 8,
-# up 5, left 5, and finally down 3:
-#
-# ...........
-# ...........
-# ...........
-# ....+----+.
-# ....|....|.
-# ....|....|.
-# ....|....|.
-# .........|.
-# .o-------+.
-# ...........
-# Then, if the second wire's path is U7,R6,D4,L4, it goes up 7, right 6, down 4, and left 4:
+# The number of steps a wire takes is the total number of grid squares the wire has entered to get to that location,
+# including the intersection being considered. Again consider the example from above:
 #
 # ...........
 # .+-----+...
@@ -38,20 +20,23 @@
 # .|.......|.
 # .o-------+.
 # ...........
+
+# In the above example, the intersection closest to the central port is reached after 8+5+5+2 = 20 steps by the first
+# wire and 7+6+4+3 = 20 steps by the second wire for a total of 20+20 = 40 steps.
 #
-# These wires cross at two locations (marked X), but the lower-left one is closer to the central port: its distance
-# is 3 + 3 = 6.
+# However, the top-right intersection is better: the first wire takes only 8+5+2 = 15 and the second wire takes only
+# 7+6+2 = 15, a total of 15+15 = 30 steps.
 #
-# Here are a few more examples:
+# Here are the best steps for the extra examples from above:
 #
 # R75,D30,R83,U83,L12,D49,R71,U7,L72
-# U62,R66,U55,R34,D71,R55,D58,R83 = distance 159
+# U62,R66,U55,R34,D71,R55,D58,R83 = 610 steps
 # R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
-# U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = distance 135
-# What is the Manhattan distance from the central port to the closest intersection?
+# U98,R91,D20,R16,D67,R40,U7,R15,U6,R7 = 410 steps
+# What is the fewest combined steps the wires must take to reach an intersection?
 #
 # Puzzle input:
-
+#
 # R999,D467,L84,D619,L49,U380,R287,U80,R744,D642,L340,U738,R959,U710,R882,U861,L130,D354,L579,D586,R798,D735,L661,
 # D453,L828,U953,R604,D834,R921,D348,R620,U775,R364,U552,L221,U119,R590,U29,L267,D745,L128,U468,L978,D717,R883,D227,
 # R691,D330,L33,U520,L862,D132,R99,U400,L455,U737,L603,U220,L689,U131,R158,D674,R617,D287,R422,U734,L73,U327,L525,
@@ -196,17 +181,48 @@ def find_distance_to_closest_crossing(list_of_crossings):
     return min_dist
 
 
-print(len(path_puzzle1))
-print(len(path_puzzle2))
+def find_shortest_distance_to_crossing(plottedmap1, plottedmap2, crossings):
+    min_steps_needed = 0
+    for crossing in crossings:
+        steps_taken = 0
+        for step in plottedmap1:
+            if crossing == step:
+                break
+            steps_taken += 1
+        for step in plottedmap2:
+            if crossing == step:
+                break
+            steps_taken += 1
+        if min_steps_needed == 0:
+            min_steps_needed = steps_taken
+        print('Steps taken:', steps_taken)
+        min_steps_needed = steps_taken if steps_taken < min_steps_needed else min_steps_needed
+    return min_steps_needed
+
+
 plot_path(path_puzzle1, wiremap1)
 plot_path(path_puzzle2, wiremap2)
-print(len(wiremap1))
-print(len(wiremap2))
-crossings = find_wire_crossings(wiremap1, wiremap2)
-for intersection in crossings:
-    print(intersection)
+
+# I still have the list of crossings from last exercise.
+# Determining it this way is very time consuming
+# So for a shortcut I hard-coded them
+# # # crossings = find_wire_crossings(wiremap1, wiremap2)
+
+crossings = [
+    {'x': 6491, 'y': 1524},
+    {'x': 6769, 'y': 1619},
+    {'x': 6788, 'y': 1619},
+    {'x': 7028, 'y': 1619},
+    {'x': 7371, 'y': 2377},
+    {'x': 7207, 'y': 2660},
+    {'x': 6518, 'y': 3060},
+    {'x': 7655, 'y': 2591}]
+
 minimum_distance = find_distance_to_closest_crossing(crossings)
-print('Minimum distance is', minimum_distance)
+print('Minimum distance is', minimum_distance, '\n')
+
+minimum_steps_needed = find_shortest_distance_to_crossing(wiremap1, wiremap2, crossings)
+print('Minimum steps needed is', minimum_steps_needed)
 
 # /usr/local/bin/python3.7 "/Users/rx05sx/Documents/Advent of Code/2019/Day3_Part1.py"
 # 301
@@ -217,15 +233,6 @@ print('Minimum distance is', minimum_distance)
 # 149380
 #
 # Find wire crossings
-# {'x': 0, 'y': 0}
-# {'x': 6491, 'y': 1524}
-# {'x': 6769, 'y': 1619}
-# {'x': 6788, 'y': 1619}
-# {'x': 7028, 'y': 1619}
-# {'x': 7371, 'y': 2377}
-# {'x': 7207, 'y': 2660}
-# {'x': 6518, 'y': 3060}
-# {'x': 7655, 'y': 2591}
 # {'x': 0, 'y': 0}
 # {'x': 6491, 'y': 1524}
 # {'x': 6769, 'y': 1619}
